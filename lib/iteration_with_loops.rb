8acefe531bb54d85f74a7e11b76d7febc4ef2910
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-21 17:26:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ def join_nested_strings(src)
     
     while inner_count < src[count].count do
       if src[count][inner_count].class == string_element.class
-      string += 
+      string " " += 
       src[count][inner_count]
       
       end
